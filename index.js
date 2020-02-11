3c58109993d44683ccbbaa2f5a5df19634afdea0
Allan C <acmlira@gmail.com>
2020-02-11 16:48:54 -0300

    Exec commands improved

--- index.js
+++ index.js
@@ -2,22 +2,34 @@
 global.__basedir = __dirname;
 
 const create = require(__basedir + '/src/create');
-const pack = require(__basedir + '/src/pack');
+const packaging = require(__basedir + '/src/packaging');
 const deploy = require(__basedir + '/src/deploy');
 
 const program = require('commander');
 
-program
-.option('-c, --create', 'create new TotalCross project')
-.option('-p, --pack', 'create package')
-.option('-d, --deploy', 'deploy your application');
-
-program.parse(process.argv);
-
 const run = async () => { 
-    if (program.create) await create();
-    if (program.pack) await pack();
-    if (program.deploy) await deploy();
+    program
+    .command('create')
+    .description('create new TotalCross project')
+    .action(async () => {
+        await create();
+    });
+
+    program
+    .command('packaging')
+    .description('create package')
+    .action(async () => {
+        await packaging();
+    });
+
+    program
+    .command('deploy')
+    .description('deploy your application')
+    .action(async () => {
+        await deploy();
+    });
+
+    await program.parse(process.argv);
 }
 
 run();
