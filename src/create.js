ed6668a372a5c8f9732ca01be637223d3ee781d1
Allan C <acmlira@gmail.com>
2020-02-11 14:48:02 -0300

    Some publish fixes

--- src/create.js
+++ src/create.js
@@ -1,25 +1,25 @@
-const interface = require('../lib/interface');
-const filesystem = require('../lib/filesystem');
-const request = require('../lib/request');
+const interface = require(__basedir + '/lib/interface');
+const filesystem = require(__basedir + '/lib/filesystem');
+const request = require(__basedir + '/lib/request');
 
 module.exports = async () => {
-    let current = await filesystem.current('./resources/maven-metadata.xml');
+    let current = await filesystem.current(__basedir + '/resources/maven-metadata.xml');
 
     let updated = await request.updated();
     
     let versions;
     if (current == updated) {
-        versions = await filesystem.versions('./resources/maven-metadata.xml');
+        versions = await filesystem.versions(__basedir + '/resources/maven-metadata.xml');
     } else {
         versions = await request.versions();
     }
 
     let response = await interface.create(versions);    
-
+    
     let path = '.';
     
     let package = path + '/src/main/java/' + response.groupId.replace(/\./g, "/");
-    
+
     filesystem.make(package);
     filesystem.make(path + '/src/main/resources');
     filesystem.make(path + '/src/test');
